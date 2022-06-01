06d26536757d85a4cce3d6db686f7a409f6ce119
davlatov8010 <davlatov.8010@gmail.com>
2022-06-01 23:49:50 +0500

    name changes

---
+++ qoshimcha.py
@@ -0,0 +1,14 @@
+a = int(input("Sonni kiriting: "))
+if a == 0:
+    print("Boshqa son kiriting:")
+
+b = a ** 2
+# print(b)
+c = b // 2 // 2 // 2
+# print(c)
+
+
+h = b / 2
+print(h)
+# q = b % 2
+# print(h , 'qodiq ' , q)
